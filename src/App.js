300dd1f6e850d98d04a9bd68ac4f04107da2f762
jaimaurya38 <maurya.jaiprakash38@gmail.com>
2023-02-21 11:07:32 +0530

    Test-1

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Modaledit from './components/Modaledit';
 import NewEntryForm from './components/NewEntryForm';
 
 function App() {
+  console.log("Run App.js");
   let [entries, setEntries] = useState(initialEntries);
 
   const [description, setDescription] = useState('');
